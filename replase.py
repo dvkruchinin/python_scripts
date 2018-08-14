feaace4456790884d1404525bc889ddf9abdfd57
dmkruchinin <dmkruchinin@yandex.ru>
2018-08-15 02:00:59 +0300

    Edit replase.py

--- replase.py
+++ replase.py
@@ -1,8 +1,10 @@
 import os, sys, shutil
 
-inputDir = "."
+inputDir = r'D:/python/for_test_replase'
 
-findStr = r"""/*
+findStr = \
+"""
+/*
     sdfwofgjkwaerfg
     wsef[ksef[p
     wefwpefkw[ef
@@ -10,29 +12,33 @@ findStr = r"""/*
     werfwkef[pwekf
     werfl,wef][,lw
     w'e;flwe][fl
-*/"""
-repStr = r"""/****************************************
-    Hi all!
-    flnfvsdogjposj pgopog gpogdfg
-    gldfgp;dfgjpodfg
-    fgldfg'dpfogk
-    w;lfsf;ksd
-/****************************************"""
-filePath = '../for_test_replase'
+*/
+"""
+repStr = \
+"""
+//****************************************
+// Hi all!
+// flnfvsdogjposj pgopog gpogdfg
+// gldfgp;dfgjpodfg
+// fgldfg'dpfogk
+// w;lfsf;ksd
+//****************************************
+"""
 
-
-def replaceStringInFile(findStr, repStr, filePath):
+def replaceStringInFile(findStr, repStr, inputDir):
     "replaces all findStr by repStr in file filePath"
-    tempName = filePath + '~~'
-    backupName = filePath + '~'
-    with open(os.path.join(filePath, 'output.cpp')) as inputFile, open(os.path.join(filePath, 'output.cpp_temp'), 'w') as outputFile:
-        inputFile = inputFile.read()
+    for root, dirs, files in os.walk(inputDir):
+        for file_name in files:
+            if file_name.endswith('.cpp'):
+                shutil.copy2(os.path.join(root, file_name), os.path.join(root, file_name + '.bak'))
+                #os.rename(os.path.join(root, file_name), os.path.join(root, file_name + '.orig'))
 
-        for thisLine in inputFile:
-            outputFile.write(thisLine.replace(findStr, repStr))
+                with open(os.path.join(root, file_name)) as inputFile, open(os.path.join(root, file_name), 'r+') as outputFile:
+                    inputFile_read = inputFile.read()
 
-    shutil.copy2(filePath, backupName)
-    os.rename(tempName, filePath)
-    print("file processed: {}".format(filePath))
+                    for line in inputFile_read:
+                        outputFile.write(line.replace(findStr, repStr))
 
-replaceStringInFile(findStr, repStr, filePath)
+                print("file processed: {}".format(root + r'/' + file_name))
+
+replaceStringInFile(findStr, repStr, inputDir)
